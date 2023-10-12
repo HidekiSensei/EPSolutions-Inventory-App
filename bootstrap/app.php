15748f27951900fc1e18b7dfe1111580f82c1151
HidekiSensei <nobington.lp@gmail.com>
2023-10-12 19:44:23 +0200

    feat() Adding CORS Handeling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
